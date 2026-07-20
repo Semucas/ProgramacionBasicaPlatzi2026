ff6ac317320e97ebdc3f7d354b18c1b08033d953
Semucas <semucas27@gmail.com>
2026-07-20 18:48:52 -0500

    Probando la depuración

--- script.js
+++ script.js
@@ -3,6 +3,28 @@ function calcularPrecio(precioUnitario, cantidad) {
     return total;
 }
 
-function puedeReservar(cantidad) {
-    return cantidad <= 2;
+function puedeReservar(tazasDisponibles) {
+    return tazasDisponibles > 0;
 }
+
+// const botonReservar = document.querySelector("#boton-reservar");
+
+// botonReservar.addEventListener("click", function () {
+//     console.log("El usuario hizo clic");
+// });
+
+
+const botonReservar = document.querySelector("#boton-reservar");
+const contadorTazas = document.querySelector("#contador-tazas");
+
+botonReservar.addEventListener("click", function () {
+    const tazasActuales = Number(contadorTazas.textContent);
+
+    if (puedeReservar(tazasActuales)) {
+        contadorTazas.textContent = tazasActuales - 1;
+    } else {
+        botonReservar.textContent = "Sin cupos";
+        alert("Ya no hay cupos")
+        botonReservar.disabled = true;
+    }
+});
